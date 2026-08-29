55e2a47541eef58b09e0a2b5051aa8b570127f42
pisti997 <zf8jc5svkd@privaterelay.appleid.com>
2026-08-29 17:41:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -221,6 +221,28 @@ function aggiungiMano() {
 
         return;
     }
+    
+    function annullaUltimoTurno() {
+
+    if (storico.length === 0) {
+        alert("Non ci sono turni da annullare.");
+        return;
+    }
+
+    const ultimoTurno = storico.pop();
+
+    for (let i = 0; i < giocatori.length; i++) {
+        punteggi[i] -= ultimoTurno.punti[i];
+    }
+
+    numeroTurno--;
+
+    document.getElementById("numero-mano").textContent =
+        "Turno " + numeroTurno;
+
+    creaTabellone();
+    mostraStorico();
+}
 
 
     if (punti < 0) {
