aef3a7cf26a2b0dff6935a97bbc6748a6e60d7d0
pisti997 <zf8jc5svkd@privaterelay.appleid.com>
2026-09-05 01:23:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    forzare il refresh della cache sui dispositivi.
 ========================================================= */
 
-const CACHE_NAME = "cardscore-cache-v11";
+const CACHE_NAME = "cardscore-cache-v12";
 
 const FILE_DA_CACHARE = [
     "./",
