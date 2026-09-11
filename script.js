5d5b8a56d4bd32c22417f7b89fb5874802356783
pisti997 <zf8jc5svkd@privaterelay.appleid.com>
2026-09-11 20:04:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2023,7 +2023,7 @@ function apriPopupInizioGame() {
     const indiceVincitore = Math.floor(Math.random() * giocatori.length);
     const nomeVincitore = giocatori[indiceVincitore];
 
-    const ALTEZZA_RIGA = 192;
+    const ALTEZZA_RIGA = 64;
     // Durate crescenti: i rulli si fermano in sequenza (effetto
     // a cascata), l'ultimo è il più lento dei tre.
     const durateRulli = [1900, 2900, 4000];
